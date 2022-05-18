35b806e88e4037d2d7a26c89878e32e0eed1ef12
miguelboing <miguel-boing@hotmail.com>
2022-05-17 23:31:16 -0300

    Implementing initial blink led example

--- main/main.c
+++ main/main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+
 #include "esp_system.h"
 #include "sdkconfig.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+
+#include "driver/gpio.h"
+
+#define BLINK_LED 2
+
 void app_main(void)
 {
-	printf("Hello world from the dog feeder!");
-	//esp_restart();
+	char *ourTaskName = pcTaskGetName(NULL);
+	ESP_LOGI(ourTaskName, "Hello, starting up!\n");
+
+	gpio_reset_pin(BLINK_LED);
+	gpio_set_direction(BLINK_LED, GPIO_MODE_OUTPUT);
+
+	while (1)
+	{
+		gpio_set_level(BLINK_LED, 1);
+		vTaskDelay(1000/ portTICK_PERIOD_MS);
+		gpio_set_level(BLINK_LED, 0);
+		vTaskDelay(1000/ portTICK_PERIOD_MS);
+	}
 }
