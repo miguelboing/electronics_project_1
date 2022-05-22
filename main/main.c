de9ccebdfd46770b152de2bed0af0f5bf6fc8152
Érico A. B. Cavalacanti <95882643+ecocaval@users.noreply.github.com>
2022-05-21 22:11:00 -0300

    Update main.c

--- main/main.c
+++ main/main.c
@@ -34,13 +34,13 @@ void app_main(void)
         button_2_is_pressed = button_is_pressed(BUTTON_2);
         button_3_is_pressed = button_is_pressed(BUTTON_3);
 
-		if (button_1_is_pressed) {ESP_LOGI(TAG, "Button 1 was pressed!\n");}
-		else 
-		if (button_2_is_pressed) {ESP_LOGI(TAG, "Button 2 was pressed!\n");}
-		else
-		if (button_3_is_pressed) {ESP_LOGI(TAG, "Button 3 was pressed!\n");}
-		else                     
-					 {ESP_LOGI(TAG, "Press a button!\n");}
+	if (button_1_is_pressed) {ESP_LOGI(TAG, "Button 1 was pressed!\n");}
+	else 
+	if (button_2_is_pressed) {ESP_LOGI(TAG, "Button 2 was pressed!\n");}
+	else
+	if (button_3_is_pressed) {ESP_LOGI(TAG, "Button 3 was pressed!\n");}
+	else                     
+				 {ESP_LOGI(TAG, "Press a button!\n");}
 
         vTaskDelay(pdMS_TO_TICKS(250));
 	}
