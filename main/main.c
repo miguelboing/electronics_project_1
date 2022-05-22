dd28d9adeca68b7b49f9cb25537eebdf06e1dbd0
Érico A. B. Cavalacanti <95882643+ecocaval@users.noreply.github.com>
2022-05-21 22:06:20 -0300

    Update main.c
    Corrigindo indentação

--- main/main.c
+++ main/main.c
@@ -30,7 +30,7 @@ void app_main(void)
 
 	while (1)
 	{
-		button_1_is_pressed = button_is_pressed(BUTTON_1); 
+	button_1_is_pressed = button_is_pressed(BUTTON_1); 
         button_2_is_pressed = button_is_pressed(BUTTON_2);
         button_3_is_pressed = button_is_pressed(BUTTON_3);
 
@@ -40,7 +40,7 @@ void app_main(void)
 		else
 		if (button_3_is_pressed) {ESP_LOGI(TAG, "Button 3 was pressed!\n");}
 		else                     
-								 {ESP_LOGI(TAG, "Press a button!\n");}
+					 {ESP_LOGI(TAG, "Press a button!\n");}
 
         vTaskDelay(pdMS_TO_TICKS(250));
 	}
