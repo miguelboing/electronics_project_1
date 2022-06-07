5bbc6e786f49ac69e1b1ab133c931a4cce172517
Miguel Boing <miguel-boing@hotmail.com>
2022-06-07 15:39:41 -0300

    Creating RTC device #5

--- main/devices/rtc/rtc.c
+++ main/devices/rtc/rtc.c
@@ -1,1 +1,28 @@
+#include <math.h>
+
 #include "rtc.h"
+
+static time_t time;
+static time_hour_t time_hour;
+static time_min_t time_min;
+
+void rtc_update_time(void)
+{
+    time += 1;
+    if (time >= 1440)
+    {
+        time = 0;
+    }
+}
+
+time_hour_t rtc_time_hour(void)
+{
+    time_hour = floor(time_hour/60);
+    return time_hour;
+}
+
+time_min_t rtc_time_min(void)
+{
+    time_min = 60 * (time % 60);
+    return time_min;
+}
--- main/devices/rtc/rtc.h
+++ main/devices/rtc/rtc.h
@@ -7,5 +7,14 @@
 
 #ifndef RTC_H_
 #define RTC_H_
+typedef unsigned int time_t;
+typedef int time_hour_t;
+typedef int time_min_t;
+
+void rtc_update_time(void);
+
+time_hour_t rtc_time_hour(void);
+
+time_min_t rtc_time_min(void);
 
 #endif /* RTC_H_ */
