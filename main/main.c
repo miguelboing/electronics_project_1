73829d80f60bd2cb13804b4165f00406c618ae52
miguelboing <miguel-boing@hotmail.com>
2022-07-05 20:18:50 -0300

    Adding load sensor lib #7

--- main/main.c
+++ main/main.c
@@ -20,11 +20,8 @@ void app_main(void)
 	char *ourTaskName = pcTaskGetName(NULL);
 	ESP_LOGI(ourTaskName, "Hello, starting up!\n");
 
-	gpio_reset_pin(BLINK_LED);
-	gpio_set_direction(BLINK_LED, GPIO_MODE_OUTPUT);
-
 	while (1)
 	{
-        Blink_lEDS();
+
 	}
 }
